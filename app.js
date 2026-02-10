8ad2d5a6eb036740c4673bd6259b87d66b03a2e7
KarenEhab53 <karenehab53@gmail.com>
2026-02-10 17:01:18 +0200

    commit

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ dbConnection();
 const Task = require("./models/Task");
 const Contact = require("./models/Contact");
 const Author = require("./models/Author");
-const Book=require("./models/Book")
+const Book = require("./models/Book");
+const Product = require("./models/Product");
 //make a post rout for to do list
 app.post("/api/tasks", async (req, res) => {
   try {
@@ -42,14 +43,15 @@ app.post("/api/tasks", async (req, res) => {
       .json({ success: false, msg: "Server error", error: err.message });
   }
 });
-
 //get tasks
 app.get("/api/tasks", async (req, res) => {
   try {
     const task = await Task.find();
+    const count = await Task.countDocuments();
     res.json({
       success: true,
       msg: "Tasks fetched successfully",
+      totalTasks: count,
       data: task,
     });
   } catch (err) {
@@ -78,9 +80,11 @@ app.post("/api/contact", async (req, res) => {
 app.get("/api/contact", async (req, res) => {
   try {
     const contact = await Contact.find();
+    const count = await Contact.countDocuments();
     res.json({
       success: true,
       msg: "Contacts fetched successfully",
+      totalContacts: count,
       data: contact,
     });
   } catch (err) {
@@ -142,46 +146,174 @@ app.post("/api/author", async (req, res) => {
     });
   }
 });
+//get authors
+app.get("/api/authors", async (req, res) => {
+  try {
+    const author = await Author.find();
+    const count = await Author.countDocuments();
+    res.json({
+      success: true,
+      msg: "Authors fetched successfully",
+      totalAuthors: count,
+      data: author,
+    });
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ success: false, msg: "Server error", error: err.message });
+  }
+});
 // post books
-app.post("/api/book",async(req,res)=>{
-  try{
-    const { title, authorId }=req.body;
-    if (!title||!authorId) {
+app.post("/api/book", async (req, res) => {
+  try {
+    const { title, authorId } = req.body;
+    if (!title || !authorId) {
       return res.status(400).json({
         success: false,
         msg: "title and author are required",
       });
     }
-const book= await Book.create({title,authorId})
-res.json({
-  success:true,
-  msg:"Book created successfullu",
-  data:book
-})
-  }catch(err){
-console.error(err);
-res.status(500).json({
-  success: false,
-  msg: "Server error",
-  error: err.message,
-});
-  }
-})
-// get book populate with author
-
-app.get("/api/books",async(req,res)=>{
-  try{
-const books= await Book.find().populate("author","name")
- res.json({
+    const book = await Book.create({ title, authorId });
+    res.json({
       success: true,
-      msg: "Books fetched successfully",
-      data: books
+      msg: "Book created successfullu",
+      data: book,
     });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ success: false, msg: "Server error", error: err.message });
+    res.status(500).json({
+      success: false,
+      msg: "Server error",
+      error: err.message,
+    });
   }
-  
-})
+});
+// get book populate with author
+app.get("/api/books", async (req, res) => {
+  try {
+    const books = await Book.find().populate("authorId", "name");
+    const count = await Book.countDocuments();
+    res.json({
+      success: true,
+      msg: "Books fetched successfully",
+      totalBooks: count,
+      data: books,
+    });
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ success: false, msg: "Server error", error: err.message });
+  }
+});
+// delete author
+app.delete("/api/author/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Book.deleteMany({ author: id });
+    const author = await Author.findByIdAndDelete(id);
+    if (!author) {
+      return res.status(404).json({ success: false, msg: "Author not found" });
+    }
+    res.json({
+      success: true,
+      msg: "Author and their books deleted successfully",
+    });
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ success: false, msg: "Server error", error: err.message });
+  }
+});
+//delete books
+app.delete("/api/book/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findByIdAndDelete(id);
+    if (!book) {
+      return res.status(404).json({ success: false, msg: "Book not found" });
+    }
+    res.json({
+      success: true,
+      msg: "Book deleted successfully",
+    });
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ success: false, msg: "Server error", error: err.message });
+  }
+});
+//post products
+app.post("/api/product", async (req, res) => {
+  try {
+    const { name, category, price } = req.body;
+    if (!name || !category || !price) {
+      return res.status(400).json({
+        success: false,
+        msg: "name , category and price are required",
+      });
+    }
+    const product = await Product.create({ name, category, price });
+    res.json({
+      success: true,
+      msg: "Product created successfully",
+      data: product,
+    });
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ success: false, msg: "Server error", error: err.message });
+  }
+});
+// get products
+app.get("/api/products", async (req, res) => {
+  try {
+    const { category } = req.query;
+
+    let filter = {};
+    if (category) {
+      filter.category = category; 
+    }
+    const products = await Product.find(filter);
+    const count= await Product.countDocuments()
+    res.json({
+      success: true,
+      msg: "Products fetched successfully",
+      totalProducts:count,
+      data: products,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      success: false,
+      msg: "Server error",
+      error: err.message,
+    });
+  }
+});
+//delete product
+app.delete("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Product.deleteMany({ author: id });
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res.status(404).json({ success: false, msg: "Product not found" });
+    }
+    res.json({
+      success: true,
+      msg: "Product deleted successfully",
+    });
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ success: false, msg: "Server error", error: err.message });
+  }
+});
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
--- models/Book.js
+++ models/Book.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 
-const bookSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  author: {
-    type: mongoose.Schema.Types.ObjectId, // store as ObjectId
-    ref: "Author", // reference to Author collection
-    required: true,
+const bookSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    authorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Author",
+      required: true,
+    },
   },
-});
+  { timestamps: true },
+);
 
 const Book = mongoose.model("Book", bookSchema);
 module.exports = Book;
